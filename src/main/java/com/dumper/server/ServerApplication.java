340a6052d479614fe09811f1618199a132292dbb
MuradTcen <muradtcen@.gmail.com>
2020-08-18 00:57:24 +0500

    added infrastructure files

---
+++ src/main/java/com/dumper/server/ServerApplication.java
@@ -0,0 +1,28 @@
+package com.dumper.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Configuration
+@EnableConfigurationProperties
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
