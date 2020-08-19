a73d479a2cebe173aaab23fd00cf9a4b56a27914
MuradTcen <muradtcen@.gmail.com>
2020-08-19 15:27:04 +0500

    added queries to BackupsetRepository

--- src/main/java/com/dumper/server/repository/BackupsetRepository.java
+++ src/main/java/com/dumper/server/repository/BackupsetRepository.java
@@ -12,4 +12,20 @@ public interface BackupsetRepository extends JpaRepository<Backupset, Long> {
             value = "select @@version",
             nativeQuery = true)
     String getVersion();
+
+
+    @Query(
+            value = "select compatibility_level from master.sys.databases where name = ?1",
+            nativeQuery = true)
+    int getCompatibilityLevel(String databaseName);
+
+    @Query(
+            value = "select user_access from master.sys.databases where name = ?1",
+            nativeQuery = true)
+    int getUserAccess(String databaseName);
+
+    @Query(
+            value = "select count(*) from sys.sysprocesses where dbid = DB_ID(N'?1')",
+            nativeQuery = true)
+    int getCountConnectionsForDatabase(String databaseName);
 }
